837b0bf691ba5c160bf2438fecf2133e7c8c4b57
Rushalee-das <76653374+Rushalee-das@users.noreply.github.com>
2024-02-13 11:01:26 +0530

    Delete Case Study/python/CarRentalSystem/entity/model directory

--- Case Study/python/CarRentalSystem/entity/model/Lease.py
+++
@@ -1,7 +0,0 @@
-class Lease:
-    def __init__(self, leaseID, vehicleID, customerID, startDate, endDate):
-        self.leaseID = leaseID
-        self.vehicleID = vehicleID
-        self.customerID = customerID
-        self.startDate = startDate
-        self.endDate = endDate
